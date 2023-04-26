1491cd3ec64de832d9db5a40a2e3cb5745e8d149
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-26 19:53:06 +0530

    Terrafrom - updating script

--- server.tf
+++ server.tf
@@ -17,6 +17,7 @@ variable "component" {
 }
 
 resource "aws_instance" "instance" {
+  count         = length(var.component)
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
