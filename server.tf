b2f20db853f9190c3715969592c4f1db62a56801
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-26 19:49:39 +0530

    Terrafrom - loops

--- server.tf
+++ server.tf
@@ -11,83 +11,17 @@ variable "instance_type"{
   default = "t3.small"
 }
 
-resource "aws_instance" "frontend" {
+variable "component" {
+  default = ["frontend","Mongodb","catalogue"]
+
+}
+
+resource "aws_instance" "instance" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = "frontend"
-  }
-}
-
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z07551482ORWS1T3ML489"
-  name    = "frontend-dev.devops71.cloud"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_instance" "redis" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "redis"
-  }
-}
-
-resource "aws_instance" "mysql" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_instance" "shipping" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_instance" "rabbitMQ" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "rabbitMQ"
-  }
-}
-
-resource "aws_instance" "payment" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "payment"
+    Name = var.component[count.index]
   }
 }
