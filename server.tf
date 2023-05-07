283a6d59a55f49d68bad45b513e05ee818bbccf0
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-05-07 18:21:30 +0530

    terraform - state dev,prod

--- server.tf
+++ server.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "instance" {
-  for_each = var.component
+  for_each = var.components
   ami           = data.aws_ami.centos.image_id
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
@@ -11,7 +11,7 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "records" {
-  for_each = var.component
+  for_each = var.components
   zone_id = "Z07551482ORWS1T3ML489"
   name    = "${each.value["name"]}-dev.devops71.cloud"
   type    = "A"
