73484c6a4fd17c8d887b1cf53693289032a155ff
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-15 22:18:48 +0530

    terraform

--- server.tf
+++ server.tf
@@ -1,8 +1,8 @@
 module "servers" {
-  for_each = var.components
-  source = "./module"
+  for_each       = var.components
+  source         = "./module"
   component_name = each.value["name"]
-  env = var.env
-  instance_type =  each_value["instance_type"]
-  password = lookup(each.value,"password","null" )
+  env            = var.env
+  instance_type  = each_value["instance_type"]
+  password       = lookup(each.value, "password", "null" )
 }
