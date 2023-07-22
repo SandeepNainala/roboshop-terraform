f58ee7c79d1fd6e6efe63fb81707bc47ba6379f2
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-22 23:29:44 +0530

    Terraform - roboshop

--- servers.tf
+++ servers.tf
@@ -8,7 +8,7 @@ module "database-servers" {
   instance_type  = each.value["instance_type"]
   password       = lookup(each.value,"password","null")
   provisioner    = true
-  app_type       = db
+  app_type       = "db"
 }
 
 module "app-servers" {
@@ -21,5 +21,5 @@ module "app-servers" {
   instance_type  = each.value["instance_type"]
   password       = lookup(each.value,"password","null")
   provisioner    = true
-  app_type       = app
+  app_type       = "app"
 }
