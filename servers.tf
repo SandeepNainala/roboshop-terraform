1c4b026f01c610e462450f51039b8cd24f17e85a
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-24 13:11:18 +0530

    Terraform - roboshop

--- servers.tf
+++ servers.tf
@@ -10,5 +10,15 @@ module "database-servers" {
   provisioner    = true
   app_type       = "db"
 }
+module "app-servers" {
+  depends_on    = [module.database-servers]
+  for_each      = var.app_servers
+  source        = "./module"
 
-
+  component_name = each.value["name"]
+  env            = "var.env"
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value,"password","null")
+  provisioner    = true
+  app_type       = "app"
+}
