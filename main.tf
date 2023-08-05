5e1e0ea2fc60c05e0d6da86dce669101890fc331
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-05 19:08:08 +0530

    Terraform - roboshop

---
+++ main.tf
@@ -0,0 +1,3 @@
+module  "vpc" {
+    source = "git::https://github.com/SandeepNainala/tf-module-vpc.git"
+}
--- module/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_type" {}
-variable "component_name" {}
-variable "password" {}
-variable "env" {}
-variable "provisioner" {
-  default = false
-}
-
-variable "app_type" {}
