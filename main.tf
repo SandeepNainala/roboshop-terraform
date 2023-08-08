41f93a55dc1ad6a1b3d8fca308858d8409ce1520
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-08 19:01:12 +0530

    Terraform - roboshop - docdb - db instance

--- main.tf
+++ main.tf
@@ -57,6 +57,8 @@ module "docdb" {
 
     allow_db_cidr = (lookup(lookup(lookup(lookup(module.vpc, "main", null),"subnets",null), each.value["allow_db_cidr"], null), "subnet_cidrs", null))
     engine_version = each.value["engine_version"]
+    instance_count = each.value["instance_count"]
+    instance_class = each.value["instance_class"]
 
 
     tags       = local.tags
