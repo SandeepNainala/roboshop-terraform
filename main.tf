85861e137eb1e5586d0ab15399d6170c7850edd7
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-09 16:01:28 +0530

    Terraform - roboshop - elasticache - security group, replication group

--- main.tf
+++ main.tf
@@ -87,5 +87,20 @@ module "rds" {
 }
 
 
+module "elasticache" {
+    source = "git::https://github.com/SandeepNainala/tf-module-elasticache.git"
+
+    for_each       = var.elasticache
+    subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+    allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+    engine_version = each.value["engine_version"]
+    replicas_per_node_group = each.value["replicas_per_node_group"]
+    num_node_group = each.value["num_node_group"]
+    node_type = each.value["node_type"]
 
 
+    tags    = local.tags
+    env     = var.env
+    vpc_id  = local.vpc_id
+    kms_arn = var.kms_arn
+}
--- vars.tf
+++ vars.tf
@@ -7,6 +7,7 @@ variable "default_vpc_cidr" {}
 variable "default_vpc_rtid" {}
 
 variable "docdb" {}
+variable "rds" {}
+variable "elasticache" {}
 
 variable "kms_arn" {}
-variable "rds" {}
