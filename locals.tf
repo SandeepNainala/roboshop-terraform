5aa83cdfcd93c231caf8f8de4aa19783d1f1b238
sandeepanainala <nainala_sandeep@yahoo.com>
2025-07-02 11:14:24 +0530

    updated the locals file for roboshop terraform

--- locals.tf
+++ locals.tf
@@ -1,10 +1,11 @@
 locals {
-  vpc_id = lookup(lookup(module.vpc, "main", null ), "vpc_id", null)
+  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)  # Fallback to null if vpc_id is not found looking for "main" key
   tags = {
     business_unit = "ecommerce"
     business_type = "retail"
-    project       = "roboshop"
-    cost_center   = 100
+    project       = "ecommerce-platform"
+    owner         = "devops-team"
     env           = var.env
+    cost_center   = 100
   }
 }
